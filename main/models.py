0c35295753b0f428baff2145dd4a8bacb3f2060a
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2023-10-26 22:16:21 +0300

    Определены модели, проведены миграции

--- main/models.py
+++ main/models.py
@@ -2,26 +2,26 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomUser(AbstractUser):
-    TYPE_CHOICES = (
-        ('doctor', 'Doctor'),
-        ('patient', 'Patient'),
-    )
-    name = models.CharField(max_length=100)
-    birth_date = models.DateField()
+class User(AbstractUser):
+    birth_date = models.DateField(null=True)
     update_date = models.DateField(auto_now=True)
     about_me = models.TextField()
-    user_type = models.CharField(choices=TYPE_CHOICES)
 
 
 class Doctor(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Patient(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
-    diagnosis = models.CharField(max_length=100)
+    diagnosis = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Assignment(models.Model):
@@ -35,8 +35,17 @@ class Assignment(models.Model):
         ('publish', 'publish'),
         ('expansion', 'expansion'),
     )
+    CATEGORY_LIST = (
+        ('favorites', 'Favorites'),
+        ('trash', 'Trash'),
+        ('library', 'Library')
+    )
     title = models.CharField(max_length=100)
     update_date = models.DateField(auto_now=True)
-    author = models.CharField(max_length=100)
-    assignment_type = models.CharField(choices=TYPE_LIST)
-    status = models.CharField(choices=STATUS_LIST)
+    author = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
+    assignment_type = models.CharField(max_length=100, choices=TYPE_LIST)
+    status = models.CharField(max_length=100, choices=STATUS_LIST)
+    category = models.CharField(max_length=100, choices=CATEGORY_LIST, default='library')
+
+    def __str__(self):
+        return self.title
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from main.forms import *
 
 
 def index(request):
