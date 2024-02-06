f1871aa9c253fee16f734197742af28a7d269428
Pavel <19418601+rakleed@users.noreply.github.com>
2024-02-06 20:45:43 +0300

    chore(front): add code formatter and linter (#7)
    Чтобы код было удобнее читать и заранее можно было бы избежать потенциальных ошибок.

---
+++ frontend/stylelint.config.js
@@ -0,0 +1,21 @@
+export default {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-idiomatic-order',
+  ],
+  ignoreFiles: [
+    'dist/**/*.css',
+  ],
+  // rules: {
+  //   'selector-class-pattern': [
+  //     '^[a-z]([-]?[a-z0-9]+)*(_[a-z0-9]([-]?[a-z0-9]+)*)?(_[a-z0-9]([-]?[a-z0-9]+)*)?' +
+  //       '(__[a-z0-9]([-]?[a-z0-9]+)*)?(_[a-z0-9]([-]?[a-z0-9]+)*)?(_[a-z0-9]([-]?[a-z0-9]+)*)?$',
+  //     {
+  //       resolveNestedSelectors: true,
+  //       message: function expected(selectorValue) {
+  //         return `Expected class selector '${selectorValue}' to match BEM CSS pattern https://bem.info/methodology/css`;
+  //       },
+  //     },
+  //   ],
+  // },
+};
