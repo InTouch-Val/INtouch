5cb51f2b960b327e02272ca37be7b5cd0d77784e
Pavel <19418601+rakleed@users.noreply.github.com>
2024-02-06 22:27:03 +0300

    chore(front): add pre-commit hook via husky

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default ({
+export default {
   plugins: [react()],
   server: {
     open: true,
@@ -11,4 +11,4 @@ export default ({
   define: {
     global: 'window',
   },
-})
+};
