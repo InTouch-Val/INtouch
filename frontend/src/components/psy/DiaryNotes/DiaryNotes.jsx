240972eadf812cb07861967a07a5844044dc302b
Kirill Karpov <rin.veber@gmail.com>
2024-04-21 17:28:31 +0400

    fix diary psy visible bag

--- frontend/src/components/psy/DiaryNotes/DiaryNotes.jsx
+++ frontend/src/components/psy/DiaryNotes/DiaryNotes.jsx
@@ -4,22 +4,35 @@ import './DiaryNotes.css';
 import CardDiary from '../CardDiary/CardDiary';
 import axios from 'axios';
 import { API } from '../../../service/axios';
+import { useObserve } from '../../../utils/hook/useObserve';
 
 export default function DiaryNotes({ clientId }) {
   const { currentUser } = useAuth();
   const [diarys, setDiarys] = React.useState();
   const [isFetching, setFetching] = React.useState(false);
 
+  const [limit, setLimit] = React.useState(0);
+
+  const [isTotal, setTotal] = React.useState(false);
+  const observeElement = React.useRef(null);
+
+  const handleTakeUpdate = React.useCallback(() => {
+    setLimit((prev) => prev + 20);
+  }, []);
+
+  useObserve(observeElement, isTotal, handleTakeUpdate);
+
   React.useEffect(() => {
-    const response = API.get('diary-notes/')
+    const response = API.get(`diary-notes/?limit=${limit}`)
       .then((res) => {
+        let data = res.data.results.filter((diary) => diary.author === clientId);
         if (res.status == 200) {
-          setDiarys(res.data.results);
+          setDiarys(data);
         }
       })
       .catch((error) => console.log(error))
       .finally(() => setFetching(true));
-  }, []);
+  }, [limit]);
 
   return (
     <section className="diarySection">
@@ -31,6 +44,7 @@ export default function DiaryNotes({ clientId }) {
               if (card.visible) return <CardDiary key={card.id} card={card} />;
             })}
       </div>
+      <div ref={observeElement} />
     </section>
   );
 }
