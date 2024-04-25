a1fc278fc025f65bd653e1d5ccb71ad478833cd7
sashadudkina5 <uu.aaa@mail.ru>
2024-04-25 16:04:02 +0400

    fixed safari mobile layout

--- frontend/stylelint.config.js
+++ frontend/stylelint.config.js
@@ -2,6 +2,7 @@ export default {
   extends: ['stylelint-config-standard', 'stylelint-config-idiomatic-order'],
   ignoreFiles: ['dist/**/*.css'],
   rules: {
+    'media-feature-range-notation': 'prefix',
     'selector-class-pattern': [
       '^[a-z]([-]?[a-z0-9]+)*(_[a-z0-9]([-]?[a-z0-9]+)*)?(_[a-z0-9]([-]?[a-z0-9]+)*)?' +
         '(__[a-z0-9]([-]?[a-z0-9]+)*)?(_[a-z0-9]([-]?[a-z0-9]+)*)?(_[a-z0-9]([-]?[a-z0-9]+)*)?$',
