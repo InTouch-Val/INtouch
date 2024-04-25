328c7cddd95b7b8fcf323e37a278a8b95a7b00a3
NechepurenkoA <nechepurenko.work@gmail.com>
2024-04-25 11:18:06 +0300

    fix for asset

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.conf import settings
 from django.contrib.auth.hashers import check_password
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
