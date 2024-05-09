4a24dd4d349ffc81d9a28e3fcc233e8ec90ef05c
Артём <coshurin.art@yandex.ru>
2024-05-09 12:24:16 +0300

    Changes after 1st review

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,14 +1,15 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
+from constants import USER_TYPES
 
 
 class IsDoctorOnly(BasePermission):
     """Пермишн, дающий права только доктору."""
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.user_type == "doctor"
+        return request.user.is_authenticated and request.user.user_type == USER_TYPES[1]
 
     def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user.user_type == "doctor"
+        return request.user.is_authenticated and request.user.user_type == USER_TYPES[1]
 
 
 class DoctorRelClient(BasePermission):
@@ -17,11 +18,12 @@ class DoctorRelClient(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            (request.user.is_authenticated and request.user.user_type == "doctor"
-             and obj.user_type == "client"
+            (request.user.is_authenticated and request.user.user_type == USER_TYPES[1]
+             and obj.user_type == USER_TYPES[0]
              and request.user.doctor.clients.filter(id=obj.client.id).exists())
             or (request.user.id == obj.id)
-            or (request.user.is_authenticated and request.user.user_type == "client"
+            or (request.user.is_authenticated and request.user.user_type == USER_TYPES[0]
+                and obj.user_type == USER_TYPES[1]
                 and obj.doctor.clients.filter(id=request.user.id).exists())
         )
 
@@ -60,7 +62,7 @@ class AssignmentDiaryDoctorOnly(BasePermission):
     редактирование - только автору задания и заметки"""
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.user_type == "doctor"
+        return request.user.is_authenticated and request.user.user_type == USER_TYPES[1]
 
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or request.user == obj.author
