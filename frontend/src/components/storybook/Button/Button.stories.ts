e462deff0410266f156b4229aa1df6133947d4ed
sashadudkina5 <uu.aaa@mail.ru>
2024-05-29 15:45:35 +0300

    first story

--- frontend/src/components/storybook/Button/Button.stories.ts
+++ frontend/src/components/storybook/Button/Button.stories.ts
@@ -1,17 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import  Button  from "./Button";
+import Button from "./Button";
 
 const meta: Meta<typeof Button> = {
-    title: 'Components/Button',
-    component: Button,
-    argTypes: {
-      backgroundColor: { control: 'color' },
-      size: {
-        control: { type: 'select', options: ['small', 'medium', 'large'] }
-      }
+  title: "Components/Button",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: "color" },
+    size: {
+      control: { type: "select", options: ["small", "medium", "large"] },
     },
-  };
+  },
+};
 
 export default meta;
 type Story = StoryObj<typeof Button>;
@@ -19,15 +19,14 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button',
+    label: "Button",
   },
 };
 
 export const Disabled: Story = {
-    args: {
-      primary: true,
-      label: 'Button',
-      disabled: true,
-    },
-  };
-  
+  args: {
+    primary: true,
+    label: "Button",
+    disabled: true,
+  },
+};
