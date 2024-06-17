3bb208e50dd9f913aa0633bdabf407ed2966a3d4
sashadudkina5 <uu.aaa@mail.ru>
2024-06-17 16:44:40 +0300

    storybook init

---
+++ frontend/src/stories/colors.ts
@@ -0,0 +1,39 @@
+const colors = {
+    primary: {
+      'Jelly Bean': '#417D88',
+      'Dim Gray': '#666565',
+      'Ori': '#FFFFFF',
+    },
+    additional: {
+      'Light Yellow': '#FFF5D6',
+      'Beige': '#F7CFA4',
+      'Light Gray': '#F5F5F5',
+      'Black': '#000000',
+      'Dark Blue': '#1F304F',
+      'White': '#FFFFFF',
+      'Gray': '#939393',
+      'Medium Gray': '#E5E5E5',
+      'Dark Gray': '#3D4452',
+    },
+    emojiColors: {
+      '100': '#417D88',
+      '200': '#71D4A6',
+      '300': '#8BD8B8',
+      '400': '#A3C1C6',
+      '500': '#B5CDD1',
+      '600': '#BAD0D4',
+      '700': '#CCDDF',
+      '800': '#E1F4F8',
+      '900': '#2C3742',
+    },
+    toastNotification: {
+      'Green': '#D2EEDB',
+      'Red': '#FFCDC',
+      'Dark Green': '#476549',
+      'Bright Red': '#FF0000',
+    },
+    scrollbar: {
+      'Gray': '#E0E0DE',
+      'Blue': '#417D88',
+    },
+  };
