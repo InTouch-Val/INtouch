66e50267eed3539eceeab406d09328933d6c6250
sashadudkina5 <uu.aaa@mail.ru>
2024-07-26 19:14:11 +0300

    changes in parseEventDetailsText

--- frontend/src/utils/helperFunction/parseEventDetailsText.ts
+++ frontend/src/utils/helperFunction/parseEventDetailsText.ts
@@ -1,16 +1,24 @@
 import { convertFromRaw, ContentState } from "draft-js";
 
+const DEFAULT_MESSAGE = "Write your answer here...";
+
 export const parseEventDetailsText = (details: string): string => {
-  let content;
-  try {
-    content = JSON.parse(details);
-    if (typeof content === "object") {
-      const contentState: ContentState = convertFromRaw(content);
-      const text: string = contentState.getPlainText();
-      return text.trim() ? text : "Write your answer here...";
+    if (typeof details !== 'string') {
+      console.warn('Expected a string for details');
+      return DEFAULT_MESSAGE;
     }
-  } catch (error) {
-    return details.trim() ? details : "Write your answer here...";
-  }
-  return "";
-};
+  
+    try {
+      const content = JSON.parse(details);
+  
+      if (typeof content === "object" && content !== null) {
+        const contentState: ContentState = convertFromRaw(content);
+        const text: string = contentState.getPlainText();
+        return text.trim() ? text : DEFAULT_MESSAGE;
+      }
+    } catch (error) {
+      console.error('Error parsing details:', error);
+    }
+  
+    return details.trim() ? details : DEFAULT_MESSAGE;
+  };
