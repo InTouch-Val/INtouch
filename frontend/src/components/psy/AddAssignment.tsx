d5034c0ec836546986d3eb02c050d2327f5754e2
sashadudkina5 <uu.aaa@mail.ru>
2024-08-06 00:54:56 +0300

    pretty

--- frontend/src/components/psy/AddAssignment.tsx
+++ frontend/src/components/psy/AddAssignment.tsx
@@ -311,7 +311,7 @@ function AddAssignment() {
     blockContainers.forEach((blockContainer, index) => {
       const blockErrorKey = `blocks #${index + 1}`;
       const blockErrorExists = Object.keys(errorMessages).some((key) =>
-        key.startsWith(blockErrorKey)
+        key.startsWith(blockErrorKey),
       );
       if (blockErrorExists) {
         blockContainer.classList.add("error");
@@ -391,7 +391,7 @@ function AddAssignment() {
     newMaxValue,
     newLeftPole,
     newRightPole,
-    newImage
+    newImage,
   ) => {
     setBlocks((prevBlocks) =>
       prevBlocks.map((block) =>
@@ -414,8 +414,8 @@ function AddAssignment() {
               rightPole: newRightPole ?? block.rightPole,
               image: newImage ?? block.image,
             }
-          : block
-      )
+          : block,
+      ),
     );
   };
 
--- frontend/src/utils/helperFunction/parseEventDetailsText.ts
+++ frontend/src/utils/helperFunction/parseEventDetailsText.ts
@@ -3,22 +3,22 @@ import { convertFromRaw, ContentState } from "draft-js";
 const DEFAULT_MESSAGE = "Write your answer here...";
 
 export const parseEventDetailsText = (details: string): string => {
-    if (typeof details !== 'string') {
-      console.warn('Expected a string for details');
-      return DEFAULT_MESSAGE;
+  if (typeof details !== "string") {
+    console.warn("Expected a string for details");
+    return DEFAULT_MESSAGE;
+  }
+
+  try {
+    const content = JSON.parse(details);
+
+    if (typeof content === "object" && content !== null) {
+      const contentState: ContentState = convertFromRaw(content);
+      const text: string = contentState.getPlainText();
+      return text.trim() ? text : DEFAULT_MESSAGE;
     }
-  
-    try {
-      const content = JSON.parse(details);
-  
-      if (typeof content === "object" && content !== null) {
-        const contentState: ContentState = convertFromRaw(content);
-        const text: string = contentState.getPlainText();
-        return text.trim() ? text : DEFAULT_MESSAGE;
-      }
-    } catch (error) {
-      console.error('Error parsing details:', error);
-    }
-  
-    return details.trim() ? details : DEFAULT_MESSAGE;
-  };
+  } catch (error) {
+    console.error("Error parsing details:", error);
+  }
+
+  return details.trim() ? details : DEFAULT_MESSAGE;
+};
