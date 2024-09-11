81c3180605a42364ae19389130b1e3bcc7fea62d
Kirill Karpov <rin.veber@gmail.com>
2024-09-12 00:55:10 +0400

    eslint

--- frontend/src/components/client/CompleteAssignments/CompleteAssignments.tsx
+++ frontend/src/components/client/CompleteAssignments/CompleteAssignments.tsx
@@ -190,14 +190,14 @@ function CompleteAssignments() {
     const multipleChoices = blocks.filter((block) => block.type === "multiple");
     if (
       multipleChoices.some(
-        (block) => !block.choice_replies.some((option) => option.checked)
+        (block) => !block.choice_replies.some((option) => option.checked),
       )
     ) {
       allFilled = false;
     }
     multipleChoices?.forEach((block) => {
       newState.multipleInputs[block.id] = block.choice_replies.some(
-        (option) => option.checked
+        (option) => option.checked,
       );
     });
 
@@ -205,14 +205,14 @@ function CompleteAssignments() {
     const singleChoices = blocks.filter((block) => block.type === "single");
     if (
       singleChoices.some(
-        (block) => !block.choice_replies.some((option) => option.checked)
+        (block) => !block.choice_replies.some((option) => option.checked),
       )
     ) {
       allFilled = false;
     }
     singleChoices?.forEach((block) => {
       newState.singleInputs[block.id] = block.choice_replies.some(
-        (option) => option.checked
+        (option) => option.checked,
       );
     });
 
@@ -220,7 +220,7 @@ function CompleteAssignments() {
     const rangeChoices = blocks.filter((block) => block.type === "range");
     if (
       rangeChoices.some(
-        (block) => block.reply === undefined || block.reply.trim() === ""
+        (block) => block.reply === undefined || block.reply.trim() === "",
       )
     ) {
       allFilled = false;
@@ -329,7 +329,7 @@ function CompleteAssignments() {
       if (res.status >= 200 && res.status < 300) {
         console.log(res.data);
         const resComplete = await API.patch(
-          `assignments-client/${assignmentData.id}/complete/`
+          `assignments-client/${assignmentData.id}/complete/`,
         );
         if (resComplete.status >= 200 && resComplete.status < 300) {
           navigate("/my-assignments");
@@ -373,7 +373,7 @@ function CompleteAssignments() {
       if (res.status >= 200 && res.status < 300) {
         console.log(res.data);
         const resComplete = await API.patch(
-          `assignments-client/${assignmentData.id}/complete/`
+          `assignments-client/${assignmentData.id}/complete/`,
         );
         if (resComplete.status >= 200 && resComplete.status < 300) {
           navigate("/my-assignments");
